995cd2443336b97d6bba76497ed8f8bf58f22738
Kimos86 <kim.oliver.sitta@gmail.com>
2017-06-13 18:19:47 +0200

    Create start.sh

--- homebridge/start.sh
+++ homebridge/start.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 ###Zusatz Berechtigung
 
-chown -R root:root /root/start.sh
-chown -R root:root /root/.homebridge/config.json
+cd /root/.homebridge
+file="/root/.homebridge/package.json"
+if [ -f "$file" ]
+then
+	echo "$file found. Going to install additional plugins."
+#	npm run install
+else
+	echo "$file not found. You can create this file to install additional plugins not already included in the docker image."
+fi
+
+chown -R root /root/start.sh
+chown -R root /root/.homebridge/config.json
+
+
 
 
 ### BEGIN INIT INFO
