0e2335763fd22390058c6dd9282bc6c72d29fd8e
Kimos86 <kim.oliver.sitta@gmail.com>
2017-06-13 19:20:38 +0200

    Create start.sh

--- homebridge/start.sh
+++ homebridge/start.sh
@@ -7,9 +7,17 @@ if [ -f "$file" ]
 then
 	echo "$file found. chown -R root /root/.homebridge/config.json"
 	chown -R root /root/.homebridge/config.json
-#	npm run install
 else
-	echo "$file not found. You can create this file to install additional plugins not already included in the docker image."
+	echo "$file not found. Problems with volumes?"
+fi
+
+file="/etc/init.d/homebridge"
+if [ -f "$file" ]
+then
+	echo "$file found. chown -R root /etc/init.d/homebridge"
+	chown -R root /etc/init.d/homebridge
+else
+	echo "$file not found. Problems with volumes?."
 fi
 
 #chown -R root /root/.homebridge/config.json
