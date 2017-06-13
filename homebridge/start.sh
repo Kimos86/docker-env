01186a03af438d7c1ba08545297e4e09156979cd
Kimos86 <kim.oliver.sitta@gmail.com>
2017-06-13 18:48:10 +0200

    Create start.sh

--- homebridge/start.sh
+++ homebridge/start.sh
@@ -2,72 +2,16 @@
 ###Zusatz Berechtigung
 
 cd /root/.homebridge
-file="/root/.homebridge/package.json"
+file="/root/.homebridge/config.json"
 if [ -f "$file" ]
 then
-	echo "$file found. Going to install additional plugins."
+	echo "$file found. chown -R root /root/.homebridge/config.json"
+	chown -R root /root/.homebridge/config.json
 #	npm run install
 else
 	echo "$file not found. You can create this file to install additional plugins not already included in the docker image."
 fi
 
-chown -R root /root/start.sh
-chown -R root /root/.homebridge/config.json
-
-
-
-
-### BEGIN INIT INFO
-# Provides: homebridge
-# Required-Start: $network $remote_fs $syslog
-# Required-Stop: $remote_fs $syslog
-# Default-Start: 2 3 4 5
-# Default-Stop: 0 1 6
-# Short-Description: Start daemon at boot time for homebridge
-# Description: Enable service provided by daemon.
-### END INIT INFO
-export PATH=$PATH:/usr/local/bin
-export NODE_PATH=$NODE_PATH:/usr/local/lib/node_modules
-PID=`pidof homebridge`
-case "$1" in
-start)
-if ps -p $PID > /dev/null 2>&1; then
-        echo "Homebridge is already running"
-else
-        su - "fhem" -c "homebridge > /dev/null 2>&1 &"
-        echo "Homebridge starting"
-        sleep 2
-    $0 status
-fi
-;;
-stop)
-if ! ps -p $PID > /dev/null 2>&1; then
-        echo "Homebridge is not running"
-else
-        kill $PID
-        echo "Homebridge closed"
-fi
-;;
-restart)
-if ! ps -p $PID > /dev/null 2>&1; then
-        $0 start
-else
-        $0 stop
-        $0 start
-fi
-;;
-status)
-if ps -p $PID > /dev/null 2>&1; then
-        echo "Homebridge is running PID $PID"
-else
-        echo "Homebridge is not running"
-fi
-;;
-*)
-echo "Usage: $0 {start|stop|status|restart}"
-exit 1
-;;
-esac
-exit 0
+#chown -R root /root/.homebridge/config.json
 
 /usr/bin/supervisord
