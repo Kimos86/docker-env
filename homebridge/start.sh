4881a8714000ba60a344b18ffc00d50022c45ef9
Kimos86 <kim.oliver.sitta@gmail.com>
2017-06-13 19:35:33 +0200

    Create start.sh

--- homebridge/start.sh
+++ homebridge/start.sh
@@ -14,8 +14,9 @@ fi
 file="/etc/init.d/homebridge"
 if [ -f "$file" ]
 then
-	echo "$file found. chown -R root /etc/init.d/homebridge"
+	echo "$file found. chown -R root /etc/init.d/homebridge && chmod 755 /etc/init.d/homebridge"
 	chown -R root /etc/init.d/homebridge
+	chmod 755 /etc/init.d/homebridge
 else
 	echo "$file not found. Problems with volumes?."
 fi
